37f0f8d2577fdc0e79f7c27c82d21c08e074c4ad
周睿 <zrufo747@outlook.com>
2024-10-28 11:27:26 +0800

    add i219 device id

--- modules/axdriver/src/drivers.rs
+++ modules/axdriver/src/drivers.rs
@@ -208,7 +208,9 @@ impl DriverProbe for E1000Driver {
         dev: Arc<pcie::Endpoint<C>>,
     ) -> Option<AxDeviceEnum> {
         let (vid, did) = dev.id();
-        if vid == 0x8086 && did == 0x10D3 {
+        let device_id_list = [0x10D3, 0x100E, 0x0DC8];
+
+        if vid == 0x8086 && device_id_list.contains( &did) {
             info!("E1000E PCI device found at {:?}", dev.address());
             // Initialize the device
             // These can be changed according to the requirments specified in the ixgbe init function.
