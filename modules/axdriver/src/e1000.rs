047594dbe66920c94d9f35c3092207fcd14d4f0b
周睿 <zrufo747@outlook.com>
2024-10-28 14:40:44 +0800

    update

--- modules/axdriver/src/e1000.rs
+++ modules/axdriver/src/e1000.rs
@@ -197,7 +197,7 @@ impl<C: Chip> KernelFunc for KFun<C> {
     }
 
     fn pci_read_config_word(&self, offset: i32) -> u16 {
-        info!("pci read config offset {}", offset);
+        // info!("pci read config offset {}", offset);
         let dword = unsafe { self.pcie.read(self.pcie.address(), offset as _) };
         let ptr = &dword as *const u32 as usize as *const u16;
         unsafe { *ptr }
